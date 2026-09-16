b4677e16eab223ea419fea9e1b781997749156d6
yunfei00 <goodman_yunfei@163.com>
2026-09-16 22:21:29 +0800

    feat: expose SmartBuilder API

--- builder/__init__.py
+++ builder/__init__.py
@@ -1,5 +1,13 @@
 """Core build engine for Smart Python Builder."""
 
 from .engine import BuildEngine, BuildRequest, BuildResult
+from .smart import EntryPointRequired, SmartBuilder, SmartBuildResult
 
-__all__ = ["BuildEngine", "BuildRequest", "BuildResult"]
+__all__ = [
+    "BuildEngine",
+    "BuildRequest",
+    "BuildResult",
+    "EntryPointRequired",
+    "SmartBuilder",
+    "SmartBuildResult",
+]
