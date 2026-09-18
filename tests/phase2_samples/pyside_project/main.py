e225ece637dbdfc06c5e30fcc61d3140b7fb4933
yunfei00 <goodman_yunfei@163.com>
2026-09-18 23:13:32 +0800

    test: remove obsolete phase acceptance samples

--- tests/phase2_samples/pyside_project/main.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QApplication, QLabel
-
-from helpers.message import message
-
-app = QApplication([])
-label = QLabel(message())
-label.resize(320, 120)
-label.show()
-app.exec()
